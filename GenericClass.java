42358c709544d70d31795e4c516cbe6588c40c73
Yi Zhou <34977713+TSBBS946894@users.noreply.github.com>
2018-02-08 19:06:59 +0800

    Add files via upload

---
+++ GenericClass.java
@@ -0,0 +1,11 @@
+class GenericClass<K,V> {
+    K key;V value;
+    GenericClass(K key,V value){this.key=key;this.value=value;}
+    static <K,V> boolean compare(GenericClass<K,V> p1,GenericClass<K,V> p2){return p1.value==p2.value;}
+    public static void main(String[] args){
+        GenericClass s1=new GenericClass<String, Integer>("apple",1);
+        GenericClass s2=new GenericClass<>(2,5);
+        System.out.println(compare(s1,s2));
+        s1.countGreaterThan(1,2);
+    }
+}
